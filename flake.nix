d78c3979ae9db0e0a51748d06d05f65dc1ca9c35
semssk <ssksem015@gmail.com>
2023-06-25 01:45:05 +0100

    Changed to flake build system

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, naersk, nixpkgs, utils, rust-overlay }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs { inherit system overlays; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            rust-bin.stable.latest.default
+            udev            
+            protobuf
+            bacon
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      });
+}
